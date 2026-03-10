87293ac76eacdcaaec252397a4bf232bc56c7c70
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2026-03-10 03:04:28 +0000

    feat: add CRSMode to exports

--- mod.types.ts
+++ mod.types.ts
@@ -1,4 +1,5 @@
 import { ulid } from '@std/ulid';
+import { CRSMode } from './lib/service/discord/CommandRegistrationService.ts';
 import { Async } from './lib/util/Async.ts';
 import { Emote } from './lib/util/baked/Emote.ts';
 import { ResponseBuilder } from './lib/util/baked/flow/ResponseBuilder.ts';
@@ -6,5 +7,6 @@ import { InternalException } from './lib/util/InternalException.ts';
 import { NativeServiceProvider, type NativeServiceProviderOptions } from './mod.provider.ts';
 import { DiscordDevkit, type DiscordDevkitNativeBrandingOptions } from './mod.ts';
 
-export { Async, DiscordDevkit, Emote, InternalException, NativeServiceProvider, ResponseBuilder, ulid };
+export { Async, CRSMode, DiscordDevkit, Emote, InternalException, NativeServiceProvider, ResponseBuilder, ulid };
 export type { DiscordDevkitNativeBrandingOptions, NativeServiceProviderOptions };
+
