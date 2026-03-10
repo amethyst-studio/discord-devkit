e261ea982508e66f50be1e775213890a7a076711
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2026-03-10 03:38:50 +0000

    feat: add BaseService and BaseChatInputCommand to exports

--- mod.types.ts
+++ mod.types.ts
@@ -1,4 +1,6 @@
 import { ulid } from '@std/ulid';
+import { BaseChatInputCommand } from './lib/base/BaseCommand.ts';
+import { BaseService } from './lib/base/BaseService.ts';
 import { CRSMode } from './lib/service/discord/CommandRegistrationService.ts';
 import { Async } from './lib/util/Async.ts';
 import { Emote } from './lib/util/baked/Emote.ts';
@@ -7,5 +9,5 @@ import { InternalException } from './lib/util/InternalException.ts';
 import { NativeServiceProvider, type NativeServiceProviderOptions } from './mod.provider.ts';
 import { DiscordDevkit, type DiscordDevkitNativeBrandingOptions } from './mod.ts';
 
-export { Async, CRSMode, DiscordDevkit, Emote, InternalException, NativeServiceProvider, ResponseBuilder, ulid };
+export { Async, BaseChatInputCommand, BaseService, CRSMode, DiscordDevkit, Emote, InternalException, NativeServiceProvider, ResponseBuilder, ulid };
 export type { DiscordDevkitNativeBrandingOptions, NativeServiceProviderOptions };
