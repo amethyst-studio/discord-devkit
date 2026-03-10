bb54d06abb6124359ad96dd2e289ea7501279825
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2026-03-10 19:42:40 +0000

    feat: add Permissions class

--- mod.types.ts
+++ mod.types.ts
@@ -4,10 +4,11 @@ import { BaseService } from './lib/base/BaseService.ts';
 import { CRSMode } from './lib/service/discord/CommandRegistrationService.ts';
 import { Async } from './lib/util/Async.ts';
 import { Emote } from './lib/util/baked/Emote.ts';
+import { Permissions } from './lib/util/baked/flow/Permissions.ts';
 import { ResponseBuilder } from './lib/util/baked/flow/ResponseBuilder.ts';
 import { InternalException } from './lib/util/InternalException.ts';
 import { NativeServiceProvider, type NativeServiceProviderOptions } from './mod.provider.ts';
 import { DiscordDevkit, type DiscordDevkitNativeBrandingOptions } from './mod.ts';
 
-export { Async, BaseChatInputCommand, BaseService, CRSMode, DiscordDevkit, Emote, InternalException, NativeServiceProvider, ResponseBuilder, ulid };
-export type { AutoCompleteHandler, AutoCompleteResponse, ComponentHandler, DiscordDevkitNativeBrandingOptions, ModalHandler, NativeServiceProviderOptions,  };
+export { Async, BaseChatInputCommand, BaseService, CRSMode, DiscordDevkit, Emote, InternalException, NativeServiceProvider, Permissions, ResponseBuilder, ulid };
+export type { AutoCompleteHandler, AutoCompleteResponse, ComponentHandler, DiscordDevkitNativeBrandingOptions, ModalHandler, NativeServiceProviderOptions };
