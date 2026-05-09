0259e59bd5e00897c995862da1531a80cc0f439c
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2026-05-09 22:08:39 +0000

    fix: correct exports

--- mod.discord.ts
+++ mod.discord.ts
@@ -7,7 +7,7 @@ export { Emote } from './lib/service/discord/baked/Emote.ts';
 export { Permissions } from './lib/service/discord/baked/flow/Permissions.ts';
 export { ResponseBuilder } from './lib/service/discord/baked/flow/ResponseBuilder.ts';
 export { BaseChatInputCommand } from './lib/service/discord/base/BaseCommand.ts';
-export type { AutoCompleteHandler, AutoCompleteResponse, ComponentHandler, ModalHandler } from './lib/service/discord/base/BaseCommand.ts';
+export type { AutoCompleteHandler, AutoCompleteResponse } from './lib/service/discord/base/BaseCommand.ts';
 export { BrandingService } from './lib/service/discord/BrandingService.ts';
 export type { BrandingServiceOptions } from './lib/service/discord/BrandingService.ts';
 export { CommandRegistrationService, CRSMode } from './lib/service/discord/CommandRegistrationService.ts';
